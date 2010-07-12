4f19e14851f25bb4d9a31e09b2a3c4b79221c26b
Ilya Petrov <ilya.muromec@gmail.com>
2010-07-12 21:42:33 +0300

    drop crap from config

--- config.py
+++ config.py
@@ -14,8 +14,3 @@ spydaap.parsers = [spydaap.parser.mp3.Mp3Parser(),
 #spydaap.media_path = "media"
 
 #spydaap.cache_dir = 'cache'
-
-spydaap.container_list.append(spydaap.playlists.Genre('reggae', ["reggae", "reggae: dub", "dub"]))
-spydaap.container_list.append(spydaap.playlists.Recent('last 30 days', (60 * 60 * 24 * 30)))
-spydaap.container_list.append(spydaap.playlists.YearRange('1970s', 1970, 1979))
-spydaap.container_list.append(spydaap.playlists.Rating('top rated', 100))
